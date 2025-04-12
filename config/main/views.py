4457ac126580bd80fa72bcb4e0640565c55ad7f6
Rustamov <rustamovasilbek1221@gmail.com>
2025-04-12 13:53:31 +0500

    djafdds

--- config/main/views.py
+++ config/main/views.py
@@ -5,18 +5,18 @@ from .serializers import FoodsSerializer, CategorySerializer, CommentSerializer
 
 class FoodsListView(APIView):
     def get(self, request):
-        foods = Foods.objects.all()  # Barcha ovqatlarni olish
+        foods = Foods.objects.all()
         serializer = FoodsSerializer(foods, many=True)
         return Response(serializer.data)
 
 class CategoryListView(APIView):
     def get(self, request):
-        categories = Category.objects.all()  # Barcha kategoriyalarni olish
+        categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
 class CommentListView(APIView):
     def get(self, request, food_id):
-        comments = Comment.objects.filter(food_id=food_id)  # Ma'lum ovqatga tegishli kommentlarni olish
+        comments = Comment.objects.filter(food_id=food_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
